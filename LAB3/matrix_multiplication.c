a2809660b5fa16f5ed1065608ffd0156d7cd007f
RohanRay015 <pinaki.bpd2001@gmail.com>
2022-03-11 10:43:49 +0530

    c

--- LAB3/matrix_multiplication.c
+++ LAB3/matrix_multiplication.c
@@ -1,49 +1,77 @@
-#include<stdio.h>    
-#include<stdlib.h>  
-int main(){  
-int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
-system("cls");  
-printf("enter the number of row=");    
-scanf("%d",&r);    
-printf("enter the number of column=");    
-scanf("%d",&c);    
-printf("enter the first matrix element=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&a[i][j]);    
-}    
-}    
-printf("enter the second matrix element=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&b[i][j]);    
-}    
-}    
+#include<stdio.h>
+int main()
+{
+    int display[100][100];
+    int first_array[100][100];
+    int second_array[100][100];
+    int multiplication_array[100][100];
+    int a,b,r,c,choice;
+    printf("Enter the number of rows :-");
+    scanf("%d",&r);
+    printf("Enter the number of columns :-");
+    scanf("%d",&c);
+    printf("1-> Display\n");
+    printf("2-> Multiplication\n");
+    printf("enter your choice\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+     case 1:   
+    for(a=0;a<r;a++)
+    {
+        for(b=0;b<c;b++)
+        {
+            printf("enter the element of array to display[%d][%d] :",a,b);
+            scanf("%d",&display[a][b]);
+        }
+    }
+    printf("the 2d array :-\n");
+    for(a=0;a<r;a++)
+    {
+        for(b=0;b<c;b++)
+        {
+            printf("%d\t",display[a][b]);
+            if(b==c-1)
+            {
+                printf("\n");
+            }
+        }
+    }
+    break;
     
-printf("multiply of the matrix=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-mul[i][j]=0;    
-for(k=0;k<c;k++)    
-{    
-mul[i][j]+=a[i][k]*b[k][j];    
-}    
-}    
-}    
-//for printing result    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-printf("%d\t",mul[i][j]);    
-}    
-printf("\n");    
-}    
-return 0;  
+
+    case 2:
+     printf("enter the value of first array\n");
+    for(a=0;a<r;a++)
+    {
+        for(b=0;b<c;b++)
+        {
+            printf("[%d][%d]",a,b);
+            scanf("%d",&first_array[a][b]);
+        }
+    }
+      printf("enter the value of second array\n");
+    for(a=0;a<r;a++)
+    {
+        for(b=0;b<c;b++)
+        {
+            printf("[%d][%d]",a,b);
+            scanf("%d",&second_array[a][b]);
+        }
+    }
+    printf("multiplication of array:-\n");
+    for(a=0;a<r;a++)
+    {
+        for(b=0;b<c;b++)
+        {
+            multiplication_array[a][b]=first_array[a][b]*second_array[a][b];
+            printf("%d\t",multiplication_array[a][b]);
+        }
+        printf("\n");
+    } 
+    break;
+    default :
+    printf("INVALID CHOICE!!!\nlease choose among the above option");
+    }
+    return 0;
 }
